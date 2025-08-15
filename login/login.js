828593c2687a98ed46ce09fd3ccf49d894e8a3e3
Felipe <felipegamae2@gmail.com>
2025-08-15 18:44:54 -0300

    commit 01 - login page

---
+++ login/login.js
@@ -0,0 +1,25 @@
+﻿const fa_lock = document.querySelector(".locker");
+
+fa_lock.addEventListener('mouseover', () => {
+   const divI = document.querySelector(".i");
+   divI.classList.toggle("teste");
+})
+
+fa_lock.addEventListener('mouseout', () => {
+   const divI = document.querySelector(".i");
+   divI.classList.toggle("teste");
+})
+
+fa_lock.addEventListener('click', () => {
+   const password = document.querySelector("#password");
+   const fa = document.querySelector(".locker");
+
+   fa.classList.toggle("fa-lock");
+   fa.classList.toggle("fa-lock-open");
+
+   if(fa.classList.contains("fa-lock-open")) {
+      password.setAttribute("type", "text");
+   } else {
+      password.setAttribute("type", "password");
+   }
+})
